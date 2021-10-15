7dee3f205992f12b810f9d35b77da82cd626622f
Beri <>
2021-10-15 09:13:21 -0700

    typo hehe

--- kubejs/startup_scripts/armor.js
+++ kubejs/startup_scripts/armor.js
@@ -19,7 +19,7 @@ let armors = [
 onEvent('item.registry.armor_tiers', event => {
     for (let [mat, d, b, e, t, k] of armors){
         // Slot indicies are [HEAD, BODY, LEGS, FEET]
-        event.add('tier_id', mat => {
+        event.add(mat, tier => {
             tier.durabilityMultiplier = d // Each slot will be multiplied with [13, 15, 16, 11]
             tier.slotProtections = [(7/12)*b, b, (3/4)*b, (1/2)*b]
             tier.enchantmentValue = e
@@ -33,10 +33,10 @@ onEvent('item.registry.armor_tiers', event => {
 
 onEvent('item.registry', event => {
     for (let [mat, d, b, e, t, k] of armors){
-        event.create(mat + "_helmet").displayName(mat[0].toUpperCase() + " Helmet").type("helmet").tier(mat)
-        event.create(mat + "_chestplate").displayName(mat[0].toUpperCase() + " Chestplate").type("chestplate").tier(mat)
-        event.create(mat + "_leggings").displayName(mat[0].toUpperCase() + " Leggings").type("leggings").tier(mat)
-        event.create(mat + "_boots").displayName(mat[0].toUpperCase() + " Boots").type("boots").tier(mat)
+        event.create(mat + "_helmet").displayName(mat[0].toUpperCase() + mat.substr(1) + " Helmet").type("helmet").tier(mat)
+        event.create(mat + "_chestplate").displayName(mat[0].toUpperCase() + mat.substr(1) + " Chestplate").type("chestplate").tier(mat)
+        event.create(mat + "_leggings").displayName(mat[0].toUpperCase() + mat.substr(1) + " Leggings").type("leggings").tier(mat)
+        event.create(mat + "_boots").displayName(mat[0].toUpperCase() + mat.substr(1) + " Boots").type("boots").tier(mat)
     }
 })
 
