a611962fd48bcedf99b72d442d6940a82b5cc347
Beri <>
2021-10-15 09:26:26 -0700

    oversight

--- kubejs/startup_scripts/armor.js
+++ kubejs/startup_scripts/armor.js
@@ -21,7 +21,7 @@ onEvent('item.registry.armor_tiers', event => {
         // Slot indicies are [HEAD, BODY, LEGS, FEET]
         event.add(mat, tier => {
             tier.durabilityMultiplier = d // Each slot will be multiplied with [13, 15, 16, 11]
-            tier.slotProtections = [Math.floor((7/12)*b), b, Math.floor((3/4)*b), Math.floor((1/2)*b)]
+            tier.slotProtections = [Math.floor((7/12)*b), Math.floor(b), Math.floor((3/4)*b), Math.floor((1/2)*b)]
             tier.enchantmentValue = e
             tier.equipSound = 'minecraft:item.armor.equip_iron'
             tier.repairIngredient = '#forge:ingots/' + mat
